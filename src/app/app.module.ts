799518f7b2052d3959cd534a176014a58ec02477
Madan patakota <pmohanreddy@firstam.com>
2022-05-08 09:32:08 +0530

    Avthar component has created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { BasicGridComponent } from './basic-grid/basic-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarComponent,
+    BasicGridComponent
   ],
   imports: [
     BrowserModule
